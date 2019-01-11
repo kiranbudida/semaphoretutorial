46e93dfcadeccc936d19fc595b94882506cac4ad
kiranindu <kiran.budida@gmail.com>
2019-01-11 10:53:12 +0530

    created PhotoStudio.java

---
+++ src/com/semaphoretutorial/PhotoStudio.java
@@ -0,0 +1,24 @@
+package com.semaphoretutorial;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+public class PhotoStudio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Semaphore studio =new Semaphore(2);
+		ExecutorService executor=Executors.newFixedThreadPool(2);
+		
+		executor.submit(new Student(studio,"Studio1"));
+		executor.submit(new Student(studio,"Studio2"));
+		executor.submit(new Student(studio,"Studio3"));
+		executor.submit(new Student(studio,"Studio4"));
+		executor.submit(new Student(studio,"Studio5"));
+		executor.submit(new Student(studio,"Studio6"));
+		
+	}
+
+}
