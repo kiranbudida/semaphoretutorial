e13589a6e49b3478a85af278c18853112d4b29d7
kiranindu <kiran.budida@gmail.com>
2019-01-11 13:03:45 +0530

    updated to PhotoStudio.java

--- src/com/semaphoretutorial/PhotoStudio.java
+++ src/com/semaphoretutorial/PhotoStudio.java
@@ -8,17 +8,15 @@ public class PhotoStudio {
 
 	public static void main(String[] args) {
 		
-
 		Semaphore studio =new Semaphore(2);
 		ExecutorService executor=Executors.newFixedThreadPool(2);
 		
-		executor.submit(new Student(studio,"Studio1"));
-		executor.submit(new Student(studio,"Studio2"));
-		executor.submit(new Student(studio,"Studio3"));
-		executor.submit(new Student(studio,"Studio4"));
-		executor.submit(new Student(studio,"Studio5"));
-		executor.submit(new Student(studio,"Studio6"));
+		executor.submit(new Student(studio,"Student1"));
+		executor.submit(new Student(studio,"Student2"));
+		executor.submit(new Student(studio,"Student3"));
+		executor.submit(new Student(studio,"Student4"));
+		executor.submit(new Student(studio,"Student5"));
+		executor.submit(new Student(studio,"Student6"));
 		
 	}
-
 }
